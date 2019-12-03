b304f0ba25df31fb4d30dc5a44e0f7b7ffd80a7f
luke.smith <luke.smith@bjss.com>
2019-12-03 14:07:46 +0000

    Slightly tidier Day 3 part 1.

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -5,11 +5,22 @@ fn main() {
 }
 
 fn generate_coords() {
+    let wire1_coords: HashSet<(i32, i32)> = calc(get_data_1());
+    let wire2_coords: HashSet<(i32, i32)> = calc(get_data_2());
+
+    let mut dupes_abs: Vec<i32> = Vec::new();
+
+    for i in wire2_coords.intersection(&wire1_coords) {
+        dupes_abs.push(i.0.abs() + i.1.abs());
+    }
+    dupes_abs.sort();
+    println!("{:?}", dupes_abs[0])
+}
+
+fn calc(data: Vec<String>) -> HashSet<(i32, i32)> {
     let mut x = 0;
     let mut y = 0;
-    let mut coords1: HashSet<(i32, i32)> = HashSet::new();
-
-    let data = get_data_1();
+    let mut coord_set: HashSet<(i32, i32)> = HashSet::new();
 
     for i in data {
         let number = i[1..].parse::<i32>().unwrap();
@@ -18,76 +29,31 @@ fn generate_coords() {
             "L" => {
                 for _ in 0..number {
                     x -= 1;
-                    coords1.insert((x, y));
+                    coord_set.insert((x, y));
                 }
             }
             "R" => {
                 for _ in 0..number {
                     x += 1;
-                    coords1.insert((x, y));
+                    coord_set.insert((x, y));
                 }
             }
             "U" => {
                 for _ in 0..number {
                     y += 1;
-                    coords1.insert((x, y));
+                    coord_set.insert((x, y));
                 }
             }
             "D" => {
                 for _ in 0..number {
                     y -= 1;
-                    coords1.insert((x, y));
+                    coord_set.insert((x, y));
                 }
             }
             _ => {}
         }
     }
-
-    x = 0;
-    y = 0;
-    let mut dupes: HashSet<(i32, i32)> = HashSet::new();
-
-    let data2 = get_data_2();
-
-    for i in data2 {
-        let number = i[1..].parse::<i32>().unwrap();
-        let letter = &i[..1];
-        match letter {
-            "L" => {
-                for _ in 0..number {
-                    x -= 1;
-                    dupes.insert((x, y));
-                }
-            }
-            "R" => {
-                for _ in 0..number {
-                    x += 1;
-                    dupes.insert((x, y));
-                }
-            }
-            "U" => {
-                for _ in 0..number {
-                    y += 1;
-                    dupes.insert((x, y));
-                }
-            }
-            "D" => {
-                for _ in 0..number {
-                    y -= 1;
-                    dupes.insert((x, y));
-                }
-            }
-            _ => {}
-        }
-    }
-
-    let mut dupes_abs: Vec<i32> = Vec::new();
-
-    for i in dupes.intersection(&coords1) {
-        dupes_abs.push(i.0.abs() + i.1.abs());
-    }
-    dupes_abs.sort();
-    println!("{:?}", dupes_abs[0])
+    coord_set
 }
 
 fn get_data_1() -> Vec<String> {
