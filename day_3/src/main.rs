16ec5a91e307e93db0300f448607821e7ae11b5f
luke.smith <luke.smith@bjss.com>
2019-12-03 19:06:26 +0000

    Day 3 part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,22 +1,40 @@
-use std::collections::HashSet;
+use indexmap::set::IndexSet;
 
 fn main() {
-    let wire1_coords: HashSet<(i32, i32)> = calc_coords_for_input(get_data_1());
-    let wire2_coords: HashSet<(i32, i32)> = calc_coords_for_input(get_data_2());
+    let wire1_coords: IndexSet<(i32, i32)> = calc_coords_for_input(get_data_1());
+    let wire2_coords: IndexSet<(i32, i32)> = calc_coords_for_input(get_data_2());
 
+    let mut dupes_coord: IndexSet<(i32, i32)> = IndexSet::new();
     let mut dupes_abs: Vec<i32> = Vec::new();
 
     for i in wire2_coords.intersection(&wire1_coords) {
+        dupes_coord.insert(*i);
         dupes_abs.push(i.0.abs() + i.1.abs());
     }
     dupes_abs.sort();
-    println!("{:?}", dupes_abs[0])
+
+    println!("Part 1 answer: {:?}", dupes_abs[0]);
+
+    let mut coords_as_summed_indices: IndexSet<usize> = IndexSet::new();
+
+    for i in &dupes_coord {
+        let wire_1_index = wire1_coords.get_full(i).unwrap().0;
+        let wire_2_index = wire2_coords.get_full(i).unwrap().0;
+
+        coords_as_summed_indices.insert(wire_1_index + 1 + wire_2_index + 1);
+    }
+
+
+    coords_as_summed_indices.sort();
+
+    println!("Part 2 answer: {:?}", coords_as_summed_indices.get_index(0).unwrap());
+
 }
 
-fn calc_coords_for_input(data: Vec<String>) -> HashSet<(i32, i32)> {
+fn calc_coords_for_input(data: Vec<String>) -> IndexSet<(i32, i32)> {
     let mut x = 0;
     let mut y = 0;
-    let mut coord_set: HashSet<(i32, i32)> = HashSet::new();
+    let mut coord_set: IndexSet<(i32, i32)> = IndexSet::new();
 
     for i in data {
         let number = i[1..].parse::<i32>().unwrap();
